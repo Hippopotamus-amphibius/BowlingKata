c78aac30689c8a61a1850c734382dc0e8648f3ab
brettmcrae <bmcrae+GitHub@bmcrae.com>
2018-12-12 11:41:56 -0700

    Test that Game is not NULL

---
+++ Game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by brettmcrae on 12/12/2018.
+//
+
+#include "Game.h"
--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #define CATCH_CONFIG_MAIN
 
 #include "catch.hpp"
+#include "Game.h"
 
-TEST_CASE("TestName","[t]")
+TEST_CASE("Game exists","[score]")
 {
-
+    auto g = new Game;
+    // Assert
+    REQUIRE(g != NULL);
 }
